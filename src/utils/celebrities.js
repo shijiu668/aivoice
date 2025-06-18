425670e5a8f49194cbf61d6b4a9e2cce43598bb2
shijiu <shijiu8861@gmail.com>
2025-06-18 18:57:47 +0800

    更新1

--- src/utils/celebrities.js
+++ src/utils/celebrities.js
@@ -3,28 +3,24 @@ export const celebrities = [
     id: 1,
     name: "Trump",
     audioFile: "/audio/Trump.wav",
-    description: "Deep, authoritative voice",
     category: "Actor"
   },
   {
     id: 2,
     name: "Hawking",
     audioFile: "/audio/Hawking.wav",
-    description: "Smooth, captivating voice",
     category: "Actress"
   },
   {
     id: 3,
     name: "Biden",
     audioFile: "/audio/baideng.wav",
-    description: "Presidential, inspiring voice",
     category: "Political"
   },
   {
     id: 4,
     name: "JoeRogan",
     audioFile: "/audio/JoeRogan.wav",
-    description: "Nature documentary narrator",
     category: "Narrator"
   }
 ];
